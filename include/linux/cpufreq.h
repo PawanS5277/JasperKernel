b3cd0e49c1893ed258580238848113a0aa675984
Ryan Andri <ryanandri@linuxmail.org>
2017-12-17 19:37:14 +0700

    [Squash] cpufreq: add latest alucard/darkness/nightmare governors
    SQUASHED
    cpufreq: alucard/darkness/nightmare: turn on suspend state function
    cpufreq: alucard, darkness, nightmare: fix checking display state
    cpufreq: alucard, nightmare: set freq responsive according min/max cluster
    cpufreq: alucard: drops bits in time calculation
    cpufreq: alucard/darkness: increase timer rate/slack
    drivers: cpufreq: drop nightmare governor
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>
    Signed-off-by: Pawan Singh <Pawan.S5277@yahoo.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -525,6 +525,12 @@ extern struct cpufreq_governor cpufreq_gov_clarity;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
