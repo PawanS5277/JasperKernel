5e0e9bd30945076029f73425e055a03e4e2b7f66
difr <dialfr@gmail.com>
2018-03-02 01:30:02 +0300

    land: core_ctl: CPU0&CPU4 offline preventing
    Signed-off-by: Pawan Singh <Pawan.S5277@yahoo.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -760,7 +760,7 @@ static void __ref do_hotplug(struct cpu_data *f)
 	mutex_lock(&lru_lock);
 	if (f->online_cpus > need) {
 		list_for_each_entry_safe(c, tmp, &f->lru, sib) {
-			if (!c->online)
+			if (!c->online || c->cpu % 4 == 0)
 				continue;
 
 			if (f->online_cpus == need)
@@ -783,7 +783,7 @@ static void __ref do_hotplug(struct cpu_data *f)
 			goto done;
 
 		list_for_each_entry_safe(c, tmp, &f->lru, sib) {
-			if (!c->online)
+			if (!c->online || c->cpu % 4 == 0)
 				continue;
 
 			if (f->online_cpus <= f->max_cpus)
