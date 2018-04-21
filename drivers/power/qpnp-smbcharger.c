89ab34a5f26d7e738362a75066b015c602635dbb
nichcream <c.potato23@gmail.com>
2018-04-21 08:55:41 +0800

    power: qpnp-smbcharger: Always report +50mV offset for voltage limits
    Allows the charger to throttle up the voltage.
    
    Signed-off-by: Pawan Singh <Pawan.S5277@yahoo.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -2290,9 +2290,12 @@ static void smbchg_parallel_usb_enable(struct smbchg_chip *chip,
 		dev_err(chip->dev,
 			"Couldn't set Vflt on parallel psy rc: %d\n", rc);
 		return;
-	} else {
-		power_supply_set_voltage_limit(chip->usb_psy,
-				(chip->vfloat_mv + 50) * 1000);
+	}
+	rc = power_supply_set_voltage_limit(chip->usb_psy,
+			(chip->vfloat_mv + 50) * 1000);
+	if (rc < 0) {
+		dev_err(chip->dev,
+			"Couldn't set Vflt on usb psy rc: %d\n", rc);
 	}
 	power_supply_set_voltage_limit(chip->usb_psy,
 			(chip->vfloat_mv + 50) * 1000);
@@ -3363,6 +3366,12 @@ static int smbchg_float_voltage_set(struct smbchg_chip *chip, int vfloat_mv)
 				rc);
 	}
 
+	rc = power_supply_set_voltage_limit(chip->usb_psy,
+				(vfloat_mv * 1000) + 50);
+	if (rc)
+		dev_err(chip->dev, "Couldn't set float voltage on usb psy rc: %d\n",
+			rc);
+
 	rc = smbchg_sec_masked_write(chip, chip->chgr_base + VFLOAT_CFG_REG,
 			VFLOAT_MASK, temp);
 
@@ -3373,9 +3382,6 @@ static int smbchg_float_voltage_set(struct smbchg_chip *chip, int vfloat_mv)
 		power_supply_set_voltage_limit(chip->usb_psy,
 				chip->vfloat_mv * 1000);
 	}
-
-	power_supply_set_voltage_limit(chip->usb_psy,
-				(chip->vfloat_mv * 1000));
 
 	return rc;
 }
